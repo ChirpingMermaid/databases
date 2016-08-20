d217c8f516fc1560ea818098116c2b7e68004ce2
chirpingmermaid <bluejello@gmail.com>
2016-08-19 20:14:44 -0700

    Create schema databases

--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,26 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT DEFAULT NULL,
+  name CHAR(31) NULL DEFAULT NULL
+);
 
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT DEFAULT NULL,
+  name CHAR(31) NULL DEFAULT NULL
+);
 
+CREATE TABLE messages (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT DEFAULT NULL,
+  user_ID INTEGER(127) NULL DEFAULT NULL,
+  room_ID INTEGER(127) NULL DEFAULT NULL,
+  message CHAR(255) NULL DEFAULT NULL,
+  FOREIGN KEY(user_ID) REFERENCES users(id),
+  FOREIGN KEY(room_ID) REFERENCES rooms(id)
+);
 
 
 /*  Execute this file from the command line by typing:
