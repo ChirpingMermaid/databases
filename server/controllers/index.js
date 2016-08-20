8e611b2222e73f9b44b3b559732063abfca69b99
chirpingmermaid <bluejello@gmail.com>
2016-08-19 21:59:13 -0700

    Working on message get function

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,9 +2,17 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
+    get: function (req, res) {
+      // console.log(req);
+      models.messages.get(req, res, function(data) {
+        console.log(data);
+        res.end(data);
+      });
+      // console.log(models.messages.get);
+    }, // a function which handles a get request for all messages
     post: function (req, res) {
       
+      exports.model.messages.post(req.data);
     } // a function which handles posting a message to the database
   },
 
--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,10 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function (req, res, callback) {
+      db.dbConnection.connect();
+      db.dbConnection.query();
+    }, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
   },
 
